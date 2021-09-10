17b9e80c7b9b9055eedbb516c5ef156dd56fab87
Chenyang Gao <gps949@outlook.com>
2021-09-10 10:59:48 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+until /app/tailscale up --authkey=${TS_AUTHKEY} --hostname=${SERVER_NAME} --accept-dns=false --advertise-routes "${JUMP_TARGET}" --reset
+do
+    sleep 0.1
+done
